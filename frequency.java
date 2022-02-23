c4c0d7671754445f9b55054c1c2b3848f3c7d049
Roshni Maria Shaji <84896587+RoshniMariaShaji@users.noreply.github.com>
2022-02-23 17:27:30 +0530

    Update frequency.java

--- frequency.java
+++ frequency.java
@@ -5,16 +5,12 @@ class frequency
 	{
 		Scanner sc=new Scanner(System.in);
 		int i,x=0;
-		System.out.println("Enter length of the string");
-		int n=sc.nextInt();
-		char s[]=new char[50];
 		System.out.println("Enter the string");
-		for(i=0;i<n;i++)
-			s[i]=sc.next().charAt(0);
+        String str=sc.nextLine();
 		System.out.println("Enter the character whose frequency is to be claculated");
 		char ch=sc.next().charAt(0);
-		for(i=0;i<n;i++)
-			if(s[i]==ch)
+		for(i=0;i<str.length();i++)
+			if(str.charAt(i)==ch)
 				x++;
 		System.out.println("Frequency of "+ch+" in the string is "+x);
 	}
